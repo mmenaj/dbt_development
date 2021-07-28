5045115eace36a186c8a308b591ba2051599e03f
mmenaj <87987532+mmenaj@users.noreply.github.com>
2021-07-28 21:37:01 +0200

    Create hub_customer.sql

---
+++ models/vault/hubs/hub_customer.sql
@@ -0,0 +1,17 @@
+{{ config(materialized='incremental') }}
+
+with source as (
+  select * from {{ ref('staging_customer') }}
+),
+
+hub_customer as (
+  select
+    md5(UPPER(C_CUSTKEY)) as CUSTOMER_KEY,
+    C_CUSTKEY as CUSTOMER_ID,
+	  sdate as loadDate,
+	  'staging_customer' as dataSource	
+  from source
+)
+select
+  *
+from hub_customer
