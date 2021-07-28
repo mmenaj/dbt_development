569c5fec79e23c2873a6ebec96ee9d0b55b73b33
mmenaj <87987532+mmenaj@users.noreply.github.com>
2021-07-28 23:54:09 +0200

    Create link_order_customer.sql

---
+++ models/vault/links/link_order_customer.sql
@@ -0,0 +1,18 @@
+{{ config(materialized='view') }}
+
+with source as (
+  select * from {{ ref('staging_orders') }}
+),
+
+link_order_customer as (
+  select
+    md5(UPPER(O_ORDERKEY) || UPPER(O_CUSTKEY)) as LINK_ORDER_CUSTOM_KEY,
+	md5(UPPER(O_ORDERKEY)) as ORDER_KEY,
+	md5(UPPER(O_CUSTKEY)) as CUSTOMER_KEY,
+    sysdate() as loadDate,
+    'staging_orders' as dataSource	
+  from source
+)
+select
+  *
+from link_order_customer
