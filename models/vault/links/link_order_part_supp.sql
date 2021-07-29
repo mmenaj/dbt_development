1c9521da46cb2c772643ed57aabc86caafeaf5cf
mmenaj <87987532+mmenaj@users.noreply.github.com>
2021-07-29 14:11:06 +0200

    Update link_order_part_supp.sql

--- models/vault/links/link_order_part_supp.sql
+++ models/vault/links/link_order_part_supp.sql
@@ -6,10 +6,12 @@ with source as (
 
 link_order_part_supp as (
   select
-    md5(UPPER(L_ORDERKEY) || UPPER(L_PARTKEY) || UPPER(L_SUPPKEY)) as LINK_ORDER_PART_SUPP_KEY,
+    md5(UPPER(L_ORDERKEY) || UPPER(L_PARTKEY) || UPPER(L_SUPPKEY) || UPPER(L_SHIPMODE) || UPPER((EXTRACT(YEAR FROM l_shipdate) || LPAD(EXTRACT(MONTH FROM l_shipdate), 2, '0')))) as LINK_ORDER_PART_SUPP_KEY,
 	md5(UPPER(L_ORDERKEY)) as ORDER_KEY,
 	md5(UPPER(L_PARTKEY)) as PART_KEY,
 	md5(UPPER(L_SUPPKEY)) as SUPPLIER_KEY,
+	md5(UPPER(L_SHIPMODE)) as SHIPMODE_KEY,
+	md5(UPPER((EXTRACT(YEAR FROM l_shipdate) || LPAD(EXTRACT(MONTH FROM l_shipdate), 2, '0')))) AS SHIPDATE_KEY,
     sysdate() as loadDate,
     'staging_lineitem' as dataSource	
   from source
