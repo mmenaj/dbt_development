a3c3d739ffcc6ea08f98e4c9d2c77de322adb590
mmenaj <87987532+mmenaj@users.noreply.github.com>
2021-07-30 00:53:29 +0200

    Update aux_datedim.sql

--- models/aux/aux_datedim.sql
+++ models/aux/aux_datedim.sql
@@ -1,8 +1,17 @@
 {{ config(materialized='table') }}
 
+
 with source as (
   select * from {{ ref('staging_datedim') }}
+),
+
+aux_datedim as (
+  select
+    *,
+	sysdate() as sdate,
+	'staging_datedim' as staging_origin	
+  from source
 )
 select
   *
-from source
+from aux_datedim
